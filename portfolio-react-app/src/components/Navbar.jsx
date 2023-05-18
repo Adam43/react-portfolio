635383aab0a947f7dfd98388e79ff220d360ea3c
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2023-05-18 14:48:45 -0400

    Resume Link updated

--- portfolio-react-app/src/components/Navbar.jsx
+++ portfolio-react-app/src/components/Navbar.jsx
@@ -135,7 +135,7 @@ const Navbar = () => {
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
             <a
               className='flex justify-between items-center w-full text-gray-300'
-              href='https://docsend.com/view/3txaxh3z3326qgpe'
+              href='https://docsend.com/view/8puwkisqpwhq2ka9'
               target='_blank'
               rel="noopener noreferrer"
             >
