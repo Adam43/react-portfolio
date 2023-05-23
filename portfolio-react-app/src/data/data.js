dd08bb0a158e6e3e416ca2a5021788acf1fecdbc
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2023-05-23 10:25:17 -0400

    removed unneeded files and added project

--- portfolio-react-app/src/data/data.js
+++ portfolio-react-app/src/data/data.js
@@ -1,13 +1,15 @@
 import WorkIm from '../assets/projects/workImg.jpeg'
 import RealEst from '../assets/projects/realestate.jpg'
+import FoodOpp from '../assets/projects/FoodOpp-img.png'
+// import SkyCart from '../assets/projects/'
 
 export const data=[
     {
         id:1,
-        name:"React JS Application",
-        image:WorkIm,
-        github:"",
-        live:"",
+        name:"FoodOpp",
+        image:FoodOpp,
+        github:"https://github.com/Adam43/FoodOpp-App",
+        live:"https://food-opp-app-2k2v.vercel.app/",
     },
     {
         id:2,
