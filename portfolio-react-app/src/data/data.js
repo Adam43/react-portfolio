6f926a6bb12f79a164ee08851a016ad56a060ac7
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2023-06-19 11:06:48 -0400

    added new project

--- portfolio-react-app/src/data/data.js
+++ portfolio-react-app/src/data/data.js
@@ -2,21 +2,30 @@
 // import RealEst from '../assets/projects/realestate.jpg'
 import FoodOpp from '../assets/projects/FoodOpp-img.png'
 import SkyCart from '../assets/projects/SkyCart-img.png'
+// import TechNiche from '../assets/projects/TechNiche-img.png'
+import TechNiche from '../assets/projects/TechNiche-s-img.png'
+
 
 export const data=[
     {
-        id:1,
+        id:3,
+        name:"Tech Niche",
+        image:TechNiche,
+        github:"https://github.com/luismolinuevo/digitaldish",
+        live:"https://digitaldish.vercel.app/",
+    },
+    {
+        id:2,
+        name:"Food Opp",
+        image:FoodOpp,
+        github:"https://github.com/Adam43/FoodOpp-App",
+        live:"https://food-opp-app-2k2v.vercel.app/",
+    },
+    {
+        id:3,
         name:"SkyCart",
         image:SkyCart,
         github:"https://github.com/robertquartey7/marketplace",
         live:"https://drive.google.com/file/d/1B4rQbhOLCTaz87XLNZweMNxF2Ga8upzj/view?usp=sharing",
     },
-    {
-        id:2,
-        name:"FoodOpp",
-        image:FoodOpp,
-        github:"https://github.com/Adam43/FoodOpp-App",
-        live:"https://food-opp-app-2k2v.vercel.app/",
-    },
-    
 ]
