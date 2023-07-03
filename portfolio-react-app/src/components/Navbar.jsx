0ba36ca2e2d0088fcc95ab6dc162d41ab9deb49c
Kwame Bullen <34757037+Adam43@users.noreply.github.com>
2023-07-03 18:55:30 -0400

    a2 logo change

--- portfolio-react-app/src/components/Navbar.jsx
+++ portfolio-react-app/src/components/Navbar.jsx
@@ -9,7 +9,7 @@ import {
 } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
-import Logo from "../assets/kb-logo1.png";
+import Logo from "../assets/kb-logo2.jpeg";
 import { Link } from 'react-scroll'; 
 
 const Navbar = () => {
@@ -17,11 +17,11 @@ const Navbar = () => {
   const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
+    <div className='fixed w-full h-[90px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       
       <div className=''>
       <Link to={"/"}> {/*Home Anchor Tag doesn't work */}
-        <img src={Logo} alt='Logo Image' style={{ width: '100px' }} /> 
+        <img src={Logo} alt='Logo Image' style={{ width: '7rem' }} /> 
       </Link>
       </div>
       
